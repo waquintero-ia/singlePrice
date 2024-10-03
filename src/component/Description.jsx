1181224af01b2dc26f620cec69b5485218dd7b34
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-10-02 23:07:40 -0500

    mobile version

--- src/component/Description.jsx
+++ src/component/Description.jsx
@@ -1,10 +1,10 @@
 const Description = () => {
-  const container__description = 'text-white bg-blue-100 opacity-70'
+  const container__description = 'text-white bg-blue-100 opacity-70 pt-[6.4vw] pr-[13.07vw] pb-[6.4vw] pl-[6.4vw]'
   const composition_h2 = 'tracking-[-0.01375rem]'
   const composition_description = 'leading-[185.72%] tracking-[-0.010625rem]'
   return (
     <>
-      <article className={`container__description ${container__description}`}>
+      <article className={`container__description flow__description__footer ${container__description}`}>
         <h2 className={`${composition_h2}`}>
           Why Us
         </h2>
--- src/component/Price.jsx
+++ src/component/Price.jsx
@@ -1,5 +1,5 @@
 const Price = () => {
-  const composition_container_price = 'text-white bg-blue-100'
+  const composition_container_price = 'text-white bg-blue-100 p-[6.4vw]'
   const composition_h2 = 'tracking-[-0.01375rem]'
   const composition_price = 'tracking-[-0.025rem]'
   const composition_unit = 'leading-[162.5%] tracking-[-0.0125rem]'
@@ -8,19 +8,23 @@ const Price = () => {
 
   return (
     <>
-      <article className={`container__price ${composition_container_price}`}>
+      <article className={`container__price flow__price ${composition_container_price}`}>
         <h2 className={`${composition_h2}`}>
           Monthly Subscription
         </h2>
-        <p className="pricing">
-          <span className={`price ${composition_price}`}>$29</span> <span className={`unit ${composition_unit} ${utility_opacity}`}>per month</span> 
-        </p>
-        <p className={`unit ${composition_unit}`}>
-          Full access for less than $1 a day
-        </p>
-        <button className={`${composition_button}`}>
-          Sign Up
-        </button>
+        <div className="container__pricing flow__pricing">
+          <div className="content__pricing flow__content__pricing">
+            <p className="pricing">
+              <span className={`price ${composition_price}`}>$29</span> <span className={`unit ${composition_unit} ${utility_opacity}`}>per month</span> 
+            </p>
+            <p className={`unit ${composition_unit}`}>
+              Full access for less than $1 a day
+            </p>
+          </div>
+          <button className={`${composition_button}`}>
+            Sign Up
+          </button>
+        </div>
       </article>
     </>
   )
